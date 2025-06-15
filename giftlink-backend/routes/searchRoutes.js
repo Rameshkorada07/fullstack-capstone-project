e638e54d6906e13b10fa5cde2470c8480eb8d582
Ramesh <vinaykorada2004@gmail.com>
2025-06-15 11:34:09 -0400

    errors resolved

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res, next) => {
 
         // Task 3: Add other filters to the query
         if (req.query.category) {
-            query.category = req.query.category
+            query.category = req.query.category;
         }
         if (req.query.condition) {
             query.condition = req.query.condition;
